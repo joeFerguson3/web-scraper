78aabc31a9b5f25b55783eb5a8610f90ca45c669
Joseph Ferguson <jjferguson1@sheffield.ac.uk>
2025-06-22 21:36:27 +0100

    find seller products

--- ebay-scraper.py
+++ ebay-scraper.py
@@ -1,3 +1,4 @@
+
 import requests
 from bs4 import BeautifulSoup
 
@@ -7,6 +8,8 @@ url = "https://www.ebay.co.uk/sch/i.html?_nkw=keyboard"
 headers = {
     'User-Agent': 'Mozilla/5.0'
 }
+sellers = []
+pending_searches = []
 
 
 # Gets sellers for a specific search
@@ -22,8 +25,34 @@ def search(title):
         price = item.select_one('.s-item__price')
         seller = item.select_one('.s-item__seller-info-text')
 
+        # Checks listing exists
         if title and price and seller:
-            print(f"{title.text.strip()} - {price.text.strip()} - {seller.text.split(' ')[0]}")
+            seller = seller.text.split(' ')[0]
+            print(f"{title.text.strip()} - {price.text.strip()} - {seller}")
+           
+            if  seller not in sellers:
+                sellers.append(seller)
+                seller_search(seller)
+    print(pending_searches)
+
+            
+
+
+def seller_search(seller):
+    url = "https://www.ebay.co.uk/sch/i.html?_dkr=1&iconV2Request=true&_blrs=recall_filtering&_ssn=" + seller
+    response = requests.get(url, headers=headers)
+    html = response.text
+
+    soup = BeautifulSoup(html, 'html.parser')
+
+    for item in soup.select('.s-item')[2:]:
+        title = item.select_one('.s-item__title')
+        price = item.select_one('.s-item__price')
+        link = item.select_one('.s-item__link')
+        url = link['href']
+
+        if title and price and link:
+            pending_searches.append(title.text)
 
 
 search("https://www.ebay.co.uk/sch/i.html?_nkw=keyboard")
