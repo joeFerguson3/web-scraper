1f94af1f9f8f93731952b10be2e89c0bfeb4dbf1
Joseph Ferguson <jjferguson1@sheffield.ac.uk>
2025-06-23 15:19:19 +0100

    updated formaitng and improved accuracy

--- ebay-scraper.py
+++ ebay-scraper.py
@@ -10,12 +10,21 @@ headers = {
     'User-Agent': 'Mozilla/5.0'
 }
 sellers = []
+
+# sellers which have been searched
+complete_sellers = []
+
 pending_searches = []
+
+# Products which have been searched
+complete_searches = {}
 session = HTMLSession()
 
 # Gets sellers for a specific search
-def search(title):
-    url = title
+def search(search):
+    print("searching for: " + search)
+    complete_searches[search] = 0
+    url = search
     response = requests.get(url, headers=headers)
     html = response.text
 
@@ -29,19 +38,25 @@ def search(title):
         product_url = product_link['href']
 
         # Checks listing exists
-        if title and price and seller and contains(product_url, "3d"):
+        if title and price and seller and contains(product_url, "3d printed"):
+           
+            # complete_searches[search] += sales(product_url)
+
+            complete_searches[search] += 1
             seller = seller.text.split(' ')[0]
             # print(f"{title.text.strip()} - {price.text.strip()} - {seller}")
            
             if seller not in sellers:
                 sellers.append(seller)
-    # print(pending_searches)
+    print(str(complete_searches[search]) + " sellers found")
+
 
             
 
 # Gets products for a paricular seller
 def seller_search(seller):
-    url = "https://www.ebay.co.uk/sch/i.html?_dkr=1&iconV2Request=true&_blrs=recall_filtering&_ssn=" + seller
+    print("Finding new listings from " + seller)
+    url = "https://www.ebay.co.uk/sch/ai.html?_dkr=1&iconV2Request=true&_blrs=recall_filtering&_ssn=" + seller
     response = requests.get(url, headers=headers)
     html = response.text
 
@@ -50,10 +65,13 @@ def seller_search(seller):
     for item in soup.select('.s-item')[2:]:
         title = item.select_one('.s-item__title')
         price = item.select_one('.s-item__price')
+        product_link = item.select_one('.s-item__link')
+        product_url = product_link['href']
         
         # Creates new product
-        if title and price:
+        if title and price and contains(product_url, "3d printed"):
             pending_searches.append(title.text)
+    print(str(len(pending_searches)) + " listings found")
 
 
 
@@ -66,17 +84,25 @@ def profile_link(text):
     return text
 
 
-# Detects key phrase in a product listing and returns num of sales
-def sales(item):
-    link = item.select_one('.s-item__link')
-    url = link['href']
-    print(url)
-    response = requests.get(url, headers=headers)
-    html = response.text
-    
-    soup = BeautifulSoup(html, 'html.parser')
-    availability = soup.select_one('#qtyAvailability')
-    # spans = availability.select('.ux-textspans.ux-textspans--SECONDARY')
+# Returns num of sales for a listing
+def sales(url):
+    try:
+        response = session.get(url)
+        response.html.render(timeout=10)
+
+        html = response.text
+
+        soup = BeautifulSoup(html, 'html.parser')
+        availability = soup.select_one('#qtyAvailability')
+
+        spans = availability.select('.ux-textspans.ux-textspans--SECONDARY')
+        number = (spans[1].text).split(" ")[0]
+        number = int(number.replace(",", ""))
+
+        print(number)
+        return number
+    except IndexError:
+        return 0
  
 # Checks product description contains given phrase (not case-sensitive)
 def contains(url, phrase):
@@ -84,7 +110,6 @@ def contains(url, phrase):
 
     # Link to product description
     desc = "https://itm.ebaydesc.com/itmdesc/" + id
-    print(desc)
 
     response = requests.get(desc, headers=headers)
     html = response.text
@@ -95,10 +120,8 @@ def contains(url, phrase):
 
     # Checks for phrase in text
     if phrase.lower() in text.lower():
-        print("true")
         return True
     else:
-        print("false")
         return False
     
 # Returns product id, extracted from given URL
@@ -111,13 +134,16 @@ def get_product_id(url):
 pending_searches.append("oral b toothbrush stand")
 # Searches sellers for first 10 products
 def run():
-    for i in range(2):
-        if len(pending_searches) == 0:
+    for i in range(100):
+        while len(pending_searches) == 0:
+            complete_sellers.append(sellers[0])
             seller_search(sellers.pop(0))
   
         search(profile_link(pending_searches.pop(0)))
    
+    print(complete_sellers)
     print(sellers)
+    print(complete_searches)
 
 run()
     
